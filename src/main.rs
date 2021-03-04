c1f46b99e9c7f895030a622fd13322d5bcbf36bc
grahamschock <gschock@gwu.edu>
2021-03-04 17:45:19 -0500

    populating student struct

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ struct Record {
     Times: String,
 }
 
+#[derive(Debug)]
 struct Student {
     fname: String,
     lname: String,
@@ -24,7 +25,22 @@ fn example() -> Result<(), Box<dyn Error>> {
         // Notice that we need to provide a type hint for automatic
         // deserialization.
         let record: Record = result?;
-        println!("{:?}", record);
+        let times = record.Times;
+        let mut split = times.split(",");
+        println!("{}", record.First_Name);
+        // for s in split {
+        //     println!("{}", s);
+        // }
+        let s1: Vec<String> = split
+            .map(|val| val.parse().unwrap())
+            .collect();
+        let student = Student {
+            fname: record.First_Name,
+            lname: record.Last_Name,
+            time: s1,
+        };
+        println!("{:?}", student);
+        // println!("{}", times);
         count = count + 1;
     }
     println!("{}", count);
