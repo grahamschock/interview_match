8d1837972c16760216f42391e3fff6fabb8a3566
grahamschock <gschock@gwu.edu>
2021-03-04 21:54:03 -0500

    printing out everything and working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::io;
 use std::process;
+use std::char;
 
 use serde::Deserialize;
 
@@ -10,41 +11,85 @@ struct Record {
     First_Name: String,
     Last_Name: String,
     Times: String,
+    isStudent: i32,
 }
 
+
 #[derive(Debug)]
-struct Student {
+struct Person {
     fname: String,
     lname: String,
     time: Vec<String>,
+    is_assigned: i32,
 }
+
+
 fn example() -> Result<(), Box<dyn Error>> {
     let mut count = 0;
     let mut rdr = csv::Reader::from_reader(io::stdin());
+    let mut students = vec![];
+    let mut instructors = vec![];
     for result in rdr.deserialize() {
         // Notice that we need to provide a type hint for automatic
         // deserialization.
         let record: Record = result?;
         let times = record.Times;
         let mut split = times.split(",");
-        println!("{}", record.First_Name);
         // for s in split {
         //     println!("{}", s);
         // }
         let s1: Vec<String> = split
             .map(|val| val.parse().unwrap())
             .collect();
-        let student = Student {
+
+        let student = Person {
             fname: record.First_Name,
             lname: record.Last_Name,
             time: s1,
+            is_assigned: 0,
         };
-        println!("{:?}", student);
-        // println!("{}", times);
+
+        if record.isStudent == 1 {
+            students.push(student);
+        } else {
+            instructors.push(student);
+        }
         count = count + 1;
     }
-    println!("{}", count);
+    students.sort_by(|a, b| a.time.len().cmp(&b.time.len()));
+    instructors.sort_by(|a,b| a.time.len().cmp(&b.time.len()));
+
+    for instructor in instructors.iter() {
+        println!("INSTRUCTOR: {}------", instructor.fname);
+        for time1 in instructor.time.iter() {
+            let index = find_student(time1.to_string(), &students);
+            if index != None {
+                students.remove(index.unwrap());
+            }
+
+        }
+    }
+    println!("-------STUDENTS-----");
+    println!("{:?}", students);
+    println!("-------INSTRUCTORS--------");
+    println!("{:?}", instructors);
+
     Ok(())
+}
+
+fn find_student(time: String, students: &Vec<Person>) -> Option<usize> {
+    let time1 = time.trim_start().to_string();
+    let mut count = 0;
+    for student in students.iter() {
+        let index = student.time.iter().position(|r| r.trim_start().eq(&time1));
+        if index != None {
+            println!("{}", student.fname);
+            println!("{}", student.time[index.unwrap()]);
+            return Some(count);
+        }
+        count = count + 1;
+    }
+    None
 }
 
 fn main() {
